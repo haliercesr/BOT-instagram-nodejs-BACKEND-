6066b6ef8675b9d98a8bd0e7ff266bd227d07b72
Cesar <haliercesr@gmail.com>
2023-11-04 10:36:20 -0300

    deploy

--- src/handlers/webhookVerifyRouterHandler.js
+++ src/handlers/webhookVerifyRouterHandler.js
@@ -5,7 +5,7 @@ const {
 
 
 const getwebhookVerifyRouterHandler=async(req,res)=>{ 
-
+ 
   // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
@@ -24,6 +24,32 @@ const getwebhookVerifyRouterHandler=async(req,res)=>{
       res.sendStatus(403);
     }
   }
+
+  try {
+   // response= await postWebhook();
+    let body = req.body ;   
+
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+
+    
+// Send a 200 OK response if this is a page webhook
+
+if (body.object === "page") {
+// Returns a '200 OK' response to all requests
+res.status(200).send("EVENT_RECEIVED");
+
+// Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+
+} else {
+// Return a '404 Not Found' if event is not from a page subscription
+res.sendStatus(404);
+}
+
+} catch (error) {
+    console.log(error.message)
+    res.status(400).json({error: error.message}); 
+}
 }
 
 module.exports={
