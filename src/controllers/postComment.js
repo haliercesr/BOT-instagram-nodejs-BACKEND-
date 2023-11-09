91b6277e0f8bbfa3cb791aab78e3cd301f91b7c6
haliercesr <haliercesr@gmail.com>
2023-11-09 13:01:04 -0300

    deploy

--- src/controllers/postComment.js
+++ src/controllers/postComment.js
@@ -13,6 +13,37 @@ const postComment= async(idcomment,idClient,time)=>{
     
     const emoji = "\u{1F604}"; // Representa la carita feliz 😄
     const message = "Olá, seu link foi enviado obrigado!"
+
+    const messagePrivate={
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://github.com/fbsamples/original-coast-clothing/blob/main/public/looks/male-work.jpg",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://www.originalcoastclothing.com",
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://www.originalcoastclothing.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
+            }
+          ]
+        }
+      }
+    }
     
 
     //DATOS DE TODOS LOS REELS PUBLICADOS
@@ -32,7 +63,7 @@ const postComment= async(idcomment,idClient,time)=>{
     //----------------------
     
     //SE ENVIA UN MENSAJE POR PRIVADO CON EL LINK
-    const response=(await axios.post(`${URLAPIfacebook}/${versionApi}/${page_id}/messages?recipient={id:${idClient}}&message={text:'Olá, deixamos o link: Dieta Flexivel https://sun.eduzz.com/1871350?a=58504888  Desafio 21 dias https://p.eduzz.com/651322?a=58504888'}&messaging_type=RESPONSE&access_token=${accessTokenPage}`)).data;
+    const response=(await axios.post(`${URLAPIfacebook}/${versionApi}/${page_id}/messages?recipient={id:${idClient}}&message=${messagePrivate}}&messaging_type=RESPONSE&access_token=${accessTokenPage}`)).data;  //'Olá, deixamos o link: Dieta Flexivel https://sun.eduzz.com/1871350?a=58504888  Desafio 21 dias https://p.eduzz.com/651322?a=58504888
     console.log(response)
 
    }
